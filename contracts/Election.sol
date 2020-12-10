d246ae5755245cf2499cd8026ba3bc720d78bc71
Aaron Bruce <aaronbruce555@gmail.com>
2020-12-10 17:05:35 -0500

    Update versions to ^0.5.16 and Update Election.sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,12 +2,35 @@ pragma solidity ^0.5.16;
 
 contract Election {
 
-  // Read/Write candidate
-  string public candidate;
+  // Candidate Model
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
 
-  // Function Constructor
+  /*
+    Read write Candidates
+    A mapping in Solidity is like an associative array or a hash, that associates key-value pairs.
+    We can create this mapping like this:
+    (Cannot iterate, uninitialized candidates will possess a default empty Candidate) Need a counter cache!
+  */
+  //      key       value    visibility
+  mapping(uint => Candidate) public candidates;
+
+  /* Store the total Number of Candidates */
+  uint public candidatesCount;
+
+  // Helper method for adding a new Candidate
+  function addCandidate(string memory _name) private {
+    candidatesCount++;  // increase the number of total candidates by 1
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);  // using the new number as an id, assign to the candidates map a new candidate using the contructor
+  }
+
+  // Constructor
   constructor () public {
-    candidate = "Candidate 1";
+    addCandidate("Guy"); 
+    addCandidate("Buddy");
   }
 
 }
